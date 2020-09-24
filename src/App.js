4cfc99980f6de7c20ee829a1cc30582798a369c1
Nikunj Sardhara <nsnikunj353@gmail.com>
2020-09-25 00:59:46 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,49 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useRef, useState } from "react";
+import mapboxgl from "mapbox-gl";
+import "mapbox-gl/dist/mapbox-gl.css";
+import data from "./data.json";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const styles = {
+  width: "100vw",
+  height: "calc(100vh - 80px)",
+  position: "absolute"
+};
+
+mapboxgl.accessToken = process.env.REACT_APP_MAPBOX_ACCESS_TOKEN;
+
+const App = () => {
+  const [map, setMap] = useState(null);
+  const mapContainer = useRef(null);
+
+  useEffect(() => {
+    const initializeMap = ({ setMap, mapContainer }) => {
+      const map = new mapboxgl.Map({
+        container: mapContainer.current,
+        style: "mapbox://styles/mapbox/streets-v11",
+        center: [72.885784, 21.215967],
+        zoom: 13
+      });
+
+      map.on("load", () => {
+        setMap(map);
+        map.resize();
+        data.map(function(dataItem){
+          var para = document.createElement("p");
+          para.style="background:white;box-shadow:0px 0px 10px black;border-radius:10px;padding:5px;";
+          para.innerHTML = dataItem.name;
+          new mapboxgl.Marker({element:para})
+          .setLngLat([dataItem.longitude, dataItem.lattitude])
+          .addTo(map);
+          return true;
+        });
+      });
+    };
+    
+
+    if (!map) initializeMap({ setMap, mapContainer });
+  }, [map]);
+
+  return <div ref={el => (mapContainer.current = el)} style={styles} />;
+};
 
 export default App;
